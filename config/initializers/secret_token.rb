3fc50101d93638b48169c8df8ccee56702f31eef
Chunhui Jiang <chunhui.jiang@aicuretechnologies.com>
2013-03-04 11:30:44 -0500

    Created scaffold lists

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captest::Application.config.secret_token = 'c4ea879f07858fb671f6265c66b351b5869af55b6fb6a289594a75bb5dd36b3e6913c3a630df241d4afe518c849fd948c7b5b1f5f3a3f3d2f2f634a39e7dcc4a'
