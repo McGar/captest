0ed88eff57aa81043300d1f3be7ba073c784dfa3
Chunhui Jiang <chunhui.jiang@aicuretechnologies.com>
2013-03-04 16:09:48 -0500

    Add credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,12 +12,15 @@ set :normalize_asset_timestamps, false
 
 set :user,            "cjiang"
 set :scm_username,    "McGar"
+set :password, "20120313"
+set :scm_passphrase, "Mg1123581321"
 # set :group,           "staff"
 set :use_sudo,        true
-set :gateway, "rubydev.aicure.com"
-role :web,    "204.13.110.73"
-role :app,    "204.13.110.73"
-role :db,     "204.13.110.73", :primary => true
+# set :gateway, "rubydev.aicure.com"
+# set :gateway, "204.13.110.73"
+role :web,    "rubydev.aicure.com"
+role :app,    "rubydev.aicure.com"
+role :db,     "rubydev.aicure.com", :primary => true
 
 set(:latest_release)  { fetch(:current_path) }
 set(:release_path)    { fetch(:current_path) }
