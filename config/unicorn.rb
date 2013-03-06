a6bd79a401f80297298645ff4d9f748a26f1d5a1
Chunhui Jiang <chunhui.jiang@aicuretechnologies.com>
2013-03-06 16:13:00 -0500

    add listen in unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ worker_processes 4
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 # listen "/tmp/captest.socket", :backlog => 64
-
+listen "/tmp/captest.socket"
 # Preload our app for more speed
 preload_app true
 
