e8a8fabd01b3035297e1ab7f7b30f67fb93802ef
Chunhui Jiang <chunhui.jiang@aicuretechnologies.com>
2013-03-12 15:17:37 -0400

    789

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-  setuid cjiang  # set to the user, which should run the app
-  exec echo 20120313 | sudo touch finished_running_unicorn
-  exec echo 20120313 | rvmsudo bundle exec unicorn_rails -l 8081 -c config/unicorn.rb -E production -D
+# setuid cjiang  # set to the user, which should run the app
+rvmsudo bundle exec unicorn_rails -l 8081 -c config/unicorn.rb -E production -D
